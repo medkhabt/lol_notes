613dcc9ece0e5baedd3a6e0686cb77e7e529d3c1
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2021-02-09 22:20:19 +0100

    initiating the project

---
+++ src/main/java/com/medkha/lol_notes/LolNotesApplication.java
@@ -0,0 +1,13 @@
+package com.medkha.lol_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medkha/lol_notes/LolNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medkha.lol_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
