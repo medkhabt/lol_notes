ffe356885e9c6a2bb00eaa29c0b6324359e29951
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2021-04-15 12:35:08 +0000

    Delete old tests

--- src/main/java/com/medkha/lol_notes/services/impl/ReasonServiceImpl.java
+++ src/main/java/com/medkha/lol_notes/services/impl/ReasonServiceImpl.java
@@ -1,6 +1,7 @@
 package com.medkha.lol_notes.services.impl;
 
 import java.util.HashSet;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.stereotype.Service;
 
 import com.medkha.lol_notes.entities.Reason;
+import com.medkha.lol_notes.exceptions.NoElementFoundException;
 import com.medkha.lol_notes.repositories.ReasonRepository;
 import com.medkha.lol_notes.services.ReasonService;
 
@@ -30,12 +32,13 @@ public class ReasonServiceImpl implements ReasonService{
 
 	@Override
 	public Reason updateReason(Reason reason) throws Exception {
-		if(existsInDataBase(reason.getId())) {
+		try {
+			findById(reason.getId()); 
 			return this.reasonRepository.save(reason); 
+		} catch (InvalidDataAccessApiUsageException | NullPointerException err) {
+			throw new IllegalArgumentException("Resaon Object is null and cannot be processed", err);
 		}
-		else { 
-			throw new Exception("Reason instance not existing ( " + reason.toString() + " ).");
-		}
+		
 	}
 
 	@Override
@@ -54,7 +57,13 @@ public class ReasonServiceImpl implements ReasonService{
 
 	@Override
 	public Reason findById(Long id) {
-		return this.reasonRepository.findById(id).orElse(null);
+		try {
+			return this.reasonRepository.findById(id).orElseThrow();			
+		} catch (InvalidDataAccessApiUsageException | NullPointerException err ) {
+			throw new IllegalArgumentException("Reason Object has null id, and cannot be processed", err); 
+		} catch (NoSuchElementException err) { 
+			throw new NoElementFoundException("No Element of type Reason with id " + id + " was found in the database.", err); 
+		}
 	}
 
 	
--- src/test/java/com/medkha/lol_notes/services/ReasonServiceTest.java
+++ src/test/java/com/medkha/lol_notes/services/ReasonServiceTest.java
@@ -1,9 +1,11 @@
 package com.medkha.lol_notes.services;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +16,7 @@ import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.medkha.lol_notes.entities.Reason;
+import com.medkha.lol_notes.exceptions.NoElementFoundException;
 import com.medkha.lol_notes.repositories.ReasonRepository;
 
 @ExtendWith(SpringExtension.class)
@@ -43,5 +46,38 @@ public class ReasonServiceTest {
 		
 		when(reasonRepositoryMock.save(reasonToCreate)).thenReturn(resultReasonSaved); 
 		assertEquals(resultReasonSaved, this.reasonService.createReason(reasonToCreate)); 
+	}
+	
+	@Test
+	public void shouldReturnIllegalArgumentException_When_ReasonIsNull_updateReason() { 
+		Reason nullIdReason = new Reason("null id"); 
+		
+		when(reasonRepositoryMock.save(null)).thenThrow(InvalidDataAccessApiUsageException.class); 
+		when(reasonRepositoryMock.findById(null)).thenThrow(NullPointerException.class);
+		
+		assertAll(
+				() -> assertThrows(IllegalArgumentException.class, ()-> {				
+					this.reasonService.updateReason(null); 
+				}), 
+				
+				() -> assertThrows(IllegalArgumentException.class, ()-> {
+					this.reasonService.updateReason(nullIdReason); 
+				})
+		); 
+	}
+	
+	@Test
+	public void shouldReturnNoElementFoundException_When_ReasonDoesntExistInDb_updateReason() { 
+		Reason reasonNoInDb = new Reason("Reason not in db"); 
+		reasonNoInDb.setId((long)1);
+		
+		when(reasonRepositoryMock.save(reasonNoInDb)).thenReturn(reasonNoInDb); 
+		when(reasonRepositoryMock.findById(reasonNoInDb.getId())).thenReturn(Optional.empty()); 
+		
+		assertThrows(NoElementFoundException.class, ()-> {
+			this.reasonService.updateReason(reasonNoInDb);
+		});
+		
+		
 	}
 }
