a49e201b5aad0bf9dfe862aa3cf9baa457faa708
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2021-04-20 23:48:38 +0000

    Remove unused imports

--- src/test/java/com/medkha/lol_notes/services/GameServiceTest.java
+++ src/test/java/com/medkha/lol_notes/services/GameServiceTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
