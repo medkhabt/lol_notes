9d1c6175c70011114fee13250ff73daf0e1b520d
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2021-05-23 21:54:06 +0100

    LN-6 add logging to the ReasonService class

--- src/main/java/com/medkha/lol_notes/services/impl/ReasonServiceImpl.java
+++ src/main/java/com/medkha/lol_notes/services/impl/ReasonServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.stereotype.Service;
@@ -16,15 +18,21 @@ import com.medkha.lol_notes.services.ReasonService;
 @Service
 public class ReasonServiceImpl implements ReasonService{
 	
+	private static final Logger log = 
+			LoggerFactory.getLogger(ReasonServiceImpl.class); 
+	
 	@Autowired
 	private ReasonRepository reasonRepository; 
 
 	@Override
 	public Reason createReason(Reason reason){
 		try {
-			return this.reasonRepository.save(reason); 
+			Reason createdReason = this.reasonRepository.save(reason);
+			log.info("createReason: Reason with id: " + createdReason.getId() + " created successfully.");
+			return  createdReason; 
 		
 		} catch (InvalidDataAccessApiUsageException | NullPointerException err) {
+			log.error("createReason: Reason Object is null and cannot be proceed");
 			throw new IllegalArgumentException("Reason Object is null and cannot be processed", err); 
 		}
 	
@@ -33,9 +41,16 @@ public class ReasonServiceImpl implements ReasonService{
 	@Override
 	public Reason updateReason(Reason reason){
 		try {
+			
 			findById(reason.getId()); 
-			return this.reasonRepository.save(reason); 
+			
+			Reason updatedReason = reasonRepository.save(reason); 
+			log.info("updateReason: Reason with id: " + updatedReason.getId() + " was updated successfully.");
+			return updatedReason; 
+			
 		} catch (InvalidDataAccessApiUsageException | NullPointerException err) {
+			
+			log.error("updateReason: Reason Object is null and cannot be proceed");
 			throw new IllegalArgumentException("Resaon Object is null and cannot be processed", err);
 		}
 		
@@ -44,23 +59,43 @@ public class ReasonServiceImpl implements ReasonService{
 	@Override
 	public void deleteReason(Long id) {
 		findById(id); 
-		this.reasonRepository.deleteById(id);
+		
+		try {
+			
+			this.reasonRepository.deleteById(id);
+			log.info("deleteReason: Reason with id: " + id + " was deleted successfully.");
+			
+		} catch (IllegalArgumentException err) {
+			log.error("deleteReason: Reason id is null, so can't proceed.");
+			throw new IllegalArgumentException("Reason id is null, so can't proceed.", err); 
+		}
 	}
 
 	@Override
 	public Set<Reason> findAllReasons() {
 		Set<Reason> findallReasonsSet = new HashSet<>(); 
 		this.reasonRepository.findAll().forEach(findallReasonsSet::add);
+		
+		log.info("findAllReasons: " + findallReasonsSet.size() + " reasons were found.");
+		
 		return findallReasonsSet;
 	}
 
 	@Override
 	public Reason findById(Long id) {
 		try {
-			return this.reasonRepository.findById(id).orElseThrow();			
+			
+			Reason foundReason = reasonRepository.findById(id).orElseThrow();
+			log.info("findById: Reason with id: " + id + " was found successfully.");
+			return foundReason; 	
+			
 		} catch (InvalidDataAccessApiUsageException | NullPointerException err ) {
+			
+			log.error("findById: Reason Object has null id, and cannot be processed");
 			throw new IllegalArgumentException("Reason Object has null id, and cannot be processed", err); 
 		} catch (NoSuchElementException err) { 
+			
+			log.error("No Element of type Reason with id " + id + " was found in the database.");
 			throw new NoElementFoundException("No Element of type Reason with id " + id + " was found in the database.", err); 
 		}
 	}
