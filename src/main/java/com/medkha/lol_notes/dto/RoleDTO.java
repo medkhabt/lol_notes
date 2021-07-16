dc34ee45e9857a805e305d9ce77c083e7ecfa94e
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2021-07-16 17:12:08 +0100

    Add unitTest for filtering deaths by role

--- src/main/java/com/medkha/lol_notes/dto/RoleDTO.java
+++ src/main/java/com/medkha/lol_notes/dto/RoleDTO.java
@@ -1,19 +1,49 @@
 package com.medkha.lol_notes.dto;
 
-public class RoleDTO {
+import java.util.Objects;
+import java.util.function.Predicate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.medkha.lol_notes.dto.interfaces.DeathFilterOption;
+
+public class RoleDTO implements DeathFilterOption {
+    private static Logger log = LoggerFactory.getLogger(RoleDTO.class);
     private String roleName;
 
     public RoleDTO(String roleName) {
-
         this.roleName = roleName;
     }
-
 
     public String getRoleName() {
         return roleName;
     }
-
     public void setRoleName(String roleName) {
         this.roleName = roleName;
+    }
+
+    @Override
+    public Predicate<DeathDTO> getPredicate() {
+        return (DeathDTO death) -> {
+            log.info("getDeathFilterByRolePredicate: Filter by Role with a name: {}", this.getRoleName());
+            Boolean result = death.getGame().getRoleName().equals(this.getRoleName());
+            log.info("Death with id: {} has Role with name: {} equals Filter Role with id:{} ? {} ",
+                    death.getId(), death.getGame().getRoleName(), this.getRoleName(), result);
+            return result;
+        };
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleDTO roleDTO = (RoleDTO) o;
+        return Objects.equals(roleName.toUpperCase(), roleDTO.roleName.toUpperCase());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleName);
     }
 }
