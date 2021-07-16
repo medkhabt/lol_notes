ef9678e22bff9be38c9899c0debfca2817ed57c8
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2021-07-16 18:03:46 +0100

    Add Filtering Deaths by Lane

--- src/main/java/com/medkha/lol_notes/dto/LaneDTO.java
+++ src/main/java/com/medkha/lol_notes/dto/LaneDTO.java
@@ -1,10 +1,15 @@
 package com.medkha.lol_notes.dto;
 
+import java.util.Objects;
 import java.util.function.Predicate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.medkha.lol_notes.dto.interfaces.DeathFilterOption;
 
 public class LaneDTO implements DeathFilterOption {
+    private static Logger log = LoggerFactory.getLogger(LaneDTO.class);
     private String laneName;
 
     public LaneDTO(String laneName) {
@@ -21,6 +26,25 @@ public class LaneDTO implements DeathFilterOption {
 
     @Override
     public Predicate<DeathDTO> getPredicate() {
-        return null;
+        return (DeathDTO death) -> {
+            log.info("getDeathFilterByLanePredicate: Filter by Lane with name: {}", this.getLaneName());
+            Boolean result = death.getGame().getLaneName().equals(this.getLaneName());
+            log.info("Death with id: {} has Lane with name: {} equals Filter Lane with id:{} ? {} ",
+                    death.getId(), death.getGame().getLaneName(), this.getLaneName(), result);
+            return result;
+        };
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LaneDTO laneDTO = (LaneDTO) o;
+        return this.laneName.equalsIgnoreCase(laneDTO.laneName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(laneName);
     }
 }
