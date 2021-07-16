9091c54a8fa1c02dee7471cb85a1548ca716d408
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2021-07-16 17:59:15 +0100

    LN-38 Add test for filtering deaths by lane

--- src/main/java/com/medkha/lol_notes/dto/LaneDTO.java
+++ src/main/java/com/medkha/lol_notes/dto/LaneDTO.java
@@ -1,6 +1,10 @@
 package com.medkha.lol_notes.dto;
 
-public class LaneDTO {
+import java.util.function.Predicate;
+
+import com.medkha.lol_notes.dto.interfaces.DeathFilterOption;
+
+public class LaneDTO implements DeathFilterOption {
     private String laneName;
 
     public LaneDTO(String laneName) {
@@ -13,5 +17,10 @@ public class LaneDTO {
 
     public void setLaneName(String laneName) {
         this.laneName = laneName;
+    }
+
+    @Override
+    public Predicate<DeathDTO> getPredicate() {
+        return null;
     }
 }
