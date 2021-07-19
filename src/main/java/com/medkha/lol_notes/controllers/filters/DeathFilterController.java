ec47289c0209098bfdfc9a23edf3d08e0d1fef0e
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2021-07-19 10:24:21 +0100

    Remove Unused services and imports, Add logging to the Controller

--- src/main/java/com/medkha/lol_notes/controllers/filters/DeathFilterController.java
+++ src/main/java/com/medkha/lol_notes/controllers/filters/DeathFilterController.java
@@ -8,8 +8,6 @@ import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -25,9 +23,6 @@ import com.medkha.lol_notes.dto.GameDTO;
 import com.medkha.lol_notes.dto.LaneDTO;
 import com.medkha.lol_notes.dto.ReasonDTO;
 import com.medkha.lol_notes.dto.RoleDTO;
-import com.medkha.lol_notes.services.DeathService;
-import com.medkha.lol_notes.services.GameService;
-import com.medkha.lol_notes.services.ReasonService;
 import com.medkha.lol_notes.services.filters.DeathFilterService;
 
 @RestController
@@ -35,13 +30,9 @@ import com.medkha.lol_notes.services.filters.DeathFilterService;
 public class DeathFilterController {
 	private static Logger log = LoggerFactory.getLogger(DeathFilterController.class);
 	private final DeathFilterService deathFilterService;
-	private final GameService gameService;
-	private final ReasonService reasonService;
 
-	public DeathFilterController(DeathFilterService deathFilterService, GameService gameService, ReasonService reasonService){
+	public DeathFilterController(DeathFilterService deathFilterService){
 		this.deathFilterService = deathFilterService;
-		this.gameService = gameService;
-		this.reasonService = reasonService;
 	}
 
 	@GetMapping(value = "/filter")
@@ -57,22 +48,27 @@ public class DeathFilterController {
 		if(gameId.isPresent()) {
 			GameDTO game = GameDTO.proxy(gameId.get());
 			deathFilterPredicates.add(game.getPredicate());
+			log.info("predicate of a game with id {} is added to the list of Predicates.", game.getId());
 		}
 		if(reasonId.isPresent()) {
 			ReasonDTO reason = ReasonDTO.proxy(reasonId.get());
 			deathFilterPredicates.add(reason.getPredicate());
+			log.info("predicate of a reason with id {} is added to the list of Predicates.", reason.getId());
 		}
 		if(championId.isPresent()) {
 			ChampionEssentielsDto champion = ChampionEssentielsDto.proxy(championId.get());
 			deathFilterPredicates.add(champion.getPredicate());
+			log.info("predicate of a champion with id {} is added to the list of Predicates.", champion.getId());
 		}
 		if(roleString.isPresent()) {
 			RoleDTO role = new RoleDTO(roleString.get());
 			deathFilterPredicates.add(role.getPredicate());
+			log.info("predicate of a role with name {} is added to the list of Predicates.", role.getRoleName());
 		}
 		if(laneString.isPresent()) {
 			LaneDTO lane = new LaneDTO(laneString.get());
 			deathFilterPredicates.add(lane.getPredicate());
+			log.info("predicate of a lane with name {} is added to the list of Predicates.", lane.getLaneName());
 		}
 		return deathFilterService.getDeathsByFilter(deathFilterPredicates).collect(Collectors.toSet());
 	}
