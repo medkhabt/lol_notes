550476ee414caafd0fcce5042339c0f60dc464f5
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2021-07-31 22:43:53 +0100

    Change endpoint name for getting queues without depr. And add new endpoint for getting queues by id

--- src/main/java/com/medkha/lol_notes/controllers/QueueController.java
+++ src/main/java/com/medkha/lol_notes/controllers/QueueController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,12 +33,20 @@ public class QueueController {
         return queueDTOSet;
     }
 
-    @GetMapping(value = "/remove-duplicate")
+    @GetMapping(value = "/remove-deprecate")
     @ResponseStatus(HttpStatus.OK)
     public Set<QueueDTO> getAllQueuesWithoutDeprecationController() {
         Set<QueueDTO> queueDTOWithoutDeprecationSet = new HashSet<>(this.queueService.getAllQueuesWithoutDeprecate());
         log.info("GetAllQueuesWithoutDeprecationController: {} queues without deprecations werer found successfully.",
                 queueDTOWithoutDeprecationSet.size());
         return queueDTOWithoutDeprecationSet;
+    }
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public QueueDTO getQueueDTOById(@PathVariable("id") Integer id) {
+        return this.queueService.getQueueById(id).orElseThrow(() -> {
+            throw new IllegalArgumentException("No Queue with id " + id + " was found.");
+        });
     }
 }
