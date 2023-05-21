61909e0ca81130560f714e53926ab26fa9284ae3
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2023-05-21 13:56:17 +0200

    Fix certification trust for riotclient and Enable consumtion of the riot live game api

---
+++ src/main/java/com/medkha/lol_notes/configuration/GeneralConfiguration.java
@@ -0,0 +1,45 @@
+package com.medkha.lol_notes.configuration;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+@Configuration
+public class GeneralConfiguration {
+
+    @Value("${http.client.ssl.trust-store}")
+    private Resource keyStore;
+    @Value("${http.client.ssl.trust-store-password}")
+    private String keyStorePassword;
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) throws IOException, CertificateException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        SSLContext sslContext = new SSLContextBuilder()
+                .loadTrustMaterial(
+                        keyStore.getURL(),
+                        keyStorePassword.toCharArray()
+                ).build();
+        SSLConnectionSocketFactory socketFactory =
+                new SSLConnectionSocketFactory(sslContext);
+        HttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(socketFactory).build();
+        HttpComponentsClientHttpRequestFactory factory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
+        return new RestTemplate(factory);
+    }
+}
--- src/main/java/com/medkha/lol_notes/controllers/GameController.java
+++ src/main/java/com/medkha/lol_notes/controllers/GameController.java
@@ -1,9 +1,13 @@
 package com.medkha.lol_notes.controllers;
 
+
 import java.util.Set;
 
 import javax.validation.Valid;
 
+import com.medkha.lol_notes.dto.PlayerDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,15 +21,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.medkha.lol_notes.dto.GameDTO;
 import com.medkha.lol_notes.services.GameService;
+import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 @RequestMapping(path="games",
 				produces="application/json")
 public class GameController {
+	private static final Logger log = LoggerFactory.getLogger(GameController.class);
 	private GameService gameService;
+	private RestTemplate restTemplate;
 	
-	public GameController(GameService gameService) {
-		this.gameService = gameService; 
+	public GameController(GameService gameService, RestTemplate restTemplate) {
+		this.gameService = gameService;
+		this.restTemplate = restTemplate;
+	}
+
+	@GetMapping(value = "live-game", produces = "application/json")
+	public int trackLiveGame() {
+		PlayerDTO activePlayer = restTemplate.getForObject("https://localhost:2999/liveclientdata/activeplayer", PlayerDTO.class);
+		log.info("Active player info: " + activePlayer.toString());
+		return 0;
 	}
 	@GetMapping(produces = "application/json")
 	public Set<GameDTO> allGames(){
