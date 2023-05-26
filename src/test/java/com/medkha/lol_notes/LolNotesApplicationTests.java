3831b242b8bc4d11842164522cf7f3878b31572f
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2023-05-26 18:54:37 +0200

    Comment the spring boot test annotation

--- src/test/java/com/medkha/lol_notes/LolNotesApplicationTests.java
+++ src/test/java/com/medkha/lol_notes/LolNotesApplicationTests.java
@@ -2,10 +2,11 @@ package com.medkha.lol_notes;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+//@SpringBootTest
 @ActiveProfiles("test")
 class LolNotesApplicationTests {
 
